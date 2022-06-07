ae5e536f7ef63261b64da7ad0241e99fc985d68a
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-06-07 21:54:08 +0530

    feat: added draggable container

---
+++ src/components/DraggableContainer.jsx
@@ -0,0 +1,19 @@
+import {usePosition} from "../contexts/positionContext";
+
+export const DraggableContainer = () => {
+  const {
+    positionState: {positionName},
+  } = usePosition();
+
+  return (
+    <div
+      className=" h-[150px] w-[150px]  text-lg font-semibold bg-stone-400 border-[1px] border-black flex flex-col justify-between relative left-0"
+      draggable="true"
+    >
+      <p className="p-1 ">
+        {positionName !== "" ? positionName : "Floating..."}
+      </p>
+      <p className="p-1 text-right text-base">Drag me around !</p>
+    </div>
+  );
+};
