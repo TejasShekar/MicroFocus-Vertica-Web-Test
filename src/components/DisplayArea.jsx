8a779c3847891adf09b33099efa97378691acebe
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-06-08 20:56:05 +0530

    fix: div position managed by radio buttons

--- src/components/DisplayArea.jsx
+++ src/components/DisplayArea.jsx
@@ -1,25 +1,19 @@
 import {useState} from "react";
-import {usePosition} from "../contexts/positionContext";
 import {DraggableContainer} from "./DraggableContainer";
 
 export const DisplayArea = () => {
   const [pointerFinal, setPointerFinal] = useState({});
 
-  const {
-    positionState: {styles},
-  } = usePosition();
-
   return (
     <main>
       <div
         className=" h-[400px] w-full bg-stone-300 flex"
-        style={styles}
         onDragOver={(e) => {
           e.preventDefault();
-          console.log({x: e.clientX, y: e.clientY});
           setPointerFinal({x: e.clientX, y: e.clientY});
         }}
         onDragEnter={(e) => e.preventDefault()}
+        // onDragLeave={(e) => console.log("left the area")}
       >
         <DraggableContainer pointerFinal={pointerFinal} />
       </div>
--- src/reducers/positionReducer.js
+++ src/reducers/positionReducer.js
@@ -1,16 +1,23 @@
-export const positionReducer = (state, {type}) => {
+export const positionReducer = (state, {type, payload}) => {
+  // console.log(payload);
   switch (type) {
     case "CENTER":
       return {
         ...state,
         positionName: "Center",
-        styles: {alignItems: "center", justifyContent: "center"},
+        styles: {top: "50%", left: "50%", transform: "translate(-50%, -50%)"},
       };
     case "LOWER_RIGHT":
       return {
         ...state,
         positionName: "Lower Right",
-        styles: {alignItems: "flex-end", justifyContent: "flex-end"},
+        styles: {bottom: "50px", right: "0"},
+      };
+    case "DRAG_POSITIONS":
+      return {
+        ...state,
+        positionName: "Floating...",
+        styles: {left: `${payload.x}px`, top: `${payload.y}px`},
       };
     default:
       return state;
