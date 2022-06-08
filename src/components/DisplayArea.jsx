bfd3104c017a383ee69bcc97df18100084cccf0f
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-06-08 19:31:02 +0530

    feat: dragging functionality
    Draggable div is now draggable across the screen.

--- src/components/DisplayArea.jsx
+++ src/components/DisplayArea.jsx
@@ -1,14 +1,28 @@
+import {useState} from "react";
 import {usePosition} from "../contexts/positionContext";
 import {DraggableContainer} from "./DraggableContainer";
 
 export const DisplayArea = () => {
-  const {positionState} = usePosition();
+  const [pointerFinal, setPointerFinal] = useState({});
+
+  const {
+    positionState: {styles},
+  } = usePosition();
+
   return (
-    <main
-      className=" h-[400px] w-full bg-stone-300 flex"
-      style={positionState.styles}
-    >
-      <DraggableContainer />
+    <main>
+      <div
+        className=" h-[400px] w-full bg-stone-300 flex"
+        style={styles}
+        onDragOver={(e) => {
+          e.preventDefault();
+          console.log({x: e.clientX, y: e.clientY});
+          setPointerFinal({x: e.clientX, y: e.clientY});
+        }}
+        onDragEnter={(e) => e.preventDefault()}
+      >
+        <DraggableContainer pointerFinal={pointerFinal} />
+      </div>
     </main>
   );
 };
--- src/components/DraggableContainer.jsx
+++ src/components/DraggableContainer.jsx
@@ -1,14 +1,31 @@
+import {useState} from "react";
 import {usePosition} from "../contexts/positionContext";
 
-export const DraggableContainer = () => {
+export const DraggableContainer = ({pointerFinal}) => {
+  const [pointerInitial, setPointerInitial] = useState({});
+  const [initialPosition, setInitialPosition] = useState({x: 0, y: 0});
+
+  const finalPosition = {
+    x: pointerFinal.x - pointerInitial.x + initialPosition.x,
+    y: pointerFinal.y - pointerInitial.y + initialPosition.y,
+  };
+
   const {
     positionState: {positionName},
   } = usePosition();
 
   return (
     <div
-      className=" h-[150px] w-[150px] text-lg font-semibold bg-stone-400 border-[1px] border-black flex flex-col justify-between"
+      className=" h-[200px] w-[200px] text-lg font-semibold bg-stone-400 border-[1px] border-black flex flex-col justify-between absolute"
       draggable="true"
+      onDragStart={(e) => {
+        setPointerInitial({x: e.clientX, y: e.clientY});
+        console.log({x: e.clientX, y: e.clientY});
+
+        let position = e.target.getBoundingClientRect();
+        setInitialPosition({x: position.x, y: position.y});
+      }}
+      style={{top: `${finalPosition.y}px`, left: `${finalPosition.x}px`}}
     >
       <p className="p-1 ">
         {positionName !== "" ? positionName : "Floating..."}
