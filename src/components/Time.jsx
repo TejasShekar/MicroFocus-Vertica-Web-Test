27de8ef622807ac6db269b08f04f818e1a53463d
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2022-06-09 20:10:37 +0530

    refactor: add cleanup function in useEffect

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,14 +1,21 @@
 import {useEffect, useState} from "react";
 
 export const Time = () => {
-  const [time, setTime] = useState("");
+  const [time, setTime] = useState(() => {
+    const date = new Date();
+    return date.toLocaleTimeString("en-GB");
+  });
+
   useEffect(() => {
-    setTimeout(() => {
+    const timerID = setInterval(() => {
       const date = new Date();
       const time = date.toLocaleTimeString("en-GB");
       setTime(time);
     }, 1000);
-  });
+    return () => {
+      clearInterval(timerID);
+    };
+  }, []);
 
   return <h3 className="text-xl font-bold">{time}</h3>;
 };
